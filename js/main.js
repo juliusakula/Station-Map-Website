5d97a1c9c0e93d7f0176099636b5da44fc1b4f29
James Ives <james@jimo.co.uk>
2015-04-08 15:50:24 -0800

    var update = 1;
    - Added pins.
    
    - Added info windows.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,59 @@
-// Anonymous function initalizes the map and takes in logic from the viewModel
-// to display pins on the map. It uses observable Arrays so the data can easily
-// be pulled for other areas of the site using data-binds.
+// The mapPin constructor class takes in logic from the View Model to display the pins you see on the map.
 
-function mapPin(name, lat, long) {
+function mapPin(name, lat, long, contentString) {
 
     this.name = ko.observable(name);
     this.lat = ko.observable(lat);
     this.long = ko.observable(long);
+    this.content = ko.observable(contentString);
+
+    // This initializes the markers on the map. It also adds an annimation too.
 
     var marker = new google.maps.Marker({
         title: name,
         position: new google.maps.LatLng(lat, long),
-        map: mapInit
-
+        map: mapView,
+        animation: google.maps.Animation.DROP
     });
 
-    var mapOptions = {
-        zoom: 12,
-        center: new google.maps.LatLng(61.196148, -149.885577)
+    // This initalizes the info window when the markers are clicked.
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    })
+
+    // This function causes the markers to bounce when they are clicked.
+
+    function toggleBounce() {
+
+        if (marker.getAnimation() != null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
     }
 
-    var mapInit = new google.maps.Map(document.getElementById('map-canvas'),
-                                  mapOptions);
+    // This calls the both functions.
+
+    google.maps.event.addListener(marker, 'click', toggleBounce);
+    google.maps.event.addListener(marker, 'click', function () {
+        infoWindow.open(mapView, marker);
+    });
 
 }
 
-// The view model takes in a name, lattitude and longitude coordinates.
+// This triggers the map to display on the screen when called on.
+
+var mapView = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 12,
+    center: new google.maps.LatLng(61.196148, -149.885577),
+});
+
+// The view model takes in a name, lattitude and longitude and data from the API.
 
 var viewModel = {
     pins: ko.observableArray([
-        new mapPin("Charlies Bakery", 61.196148, -149.885577),
+        new mapPin("Charlies Bakery", 61.196148, -149.885577, "Testinglol"),
         new mapPin("Moose's Tooth", 61.190491, -149.868937)
     ])
 };
