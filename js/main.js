d69fcee43c41b5c21b8602e7f945fa93ddaade46
James Ives <james@jimo.co.uk>
2015-04-08 18:45:08 -0800

    var update = 3;
    - Made some adjustments.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 // The mapPin constructor class takes in logic from the View Model to display the pins you see on the map.
 
-function mapPin(name, lat, long, contentString) {
+function mapPin(name, lat, long) {
 
     this.name = ko.observable(name);
     this.lat = ko.observable(lat);
     this.long = ko.observable(long);
-    this.content = ko.observable(contentString);
 
     // This initializes the markers on the map. It also adds an annimation too.
 
@@ -15,10 +14,12 @@ function mapPin(name, lat, long, contentString) {
         animation: google.maps.Animation.DROP
     });
 
-    // This initalizes the info window when the markers are clicked.
+    // This initalizes the info window when the markers are clicked. It also creates a div for the API data.
+
+    var contentString = '<div id="wikiData"></div>';
 
     var infoWindow = new google.maps.InfoWindow({
-        content: name + lat + long
+        content: contentString
 
     })
 
@@ -33,11 +34,40 @@ function mapPin(name, lat, long, contentString) {
         }
     }
 
-    // This calls the both functions.
+    // This function pulls in data.
+
+    function apiData(name) {
+
+        var wikipediaURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + name + '&format=json&callback=wikiCallback';
+        var wikiRequestTimeout = setTimeout(function () {
+            wikiData.text("Failed to get Wikipedia resources");
+        }, 8000);
+
+        $.ajax({
+
+            url: wikipediaURL,
+            dataType: "jsonp",
+
+            success: function (response) {
+                var articleList = response[1];
+
+                for (var i = 0; i < articleList.length; i++) {
+                    articleStr = articleList[i];
+                    var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                    wikiData.append('<li><a href ="' + url + '">' + articleStr + '</a></li>');
+                };
+
+                clearTimeout(wikiRequestTimeout);
+            }
+        });
+
+    }
+
+    // Calls the animations and info window.
     
     google.maps.event.addListener(marker, 'click', toggleBounce);
     google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.open(mapView, marker);
+        infoWindow.open(mapView, marker, apiData);
     });
 
 }
@@ -49,46 +79,15 @@ var mapView = new google.maps.Map(document.getElementById('map-canvas'), {
     center: new google.maps.LatLng(61.196148, -149.885577),
 });
 
-// The view model takes in a name, lattitude and longitude and data from the API.
+// The view model takes in a name, lattitude and longitude.
 
 var viewModel = {
     pins: ko.observableArray([
-        new mapPin("Charlies Bakery", 61.196148, -149.885577, content),
-        new mapPin("Moose's Tooth", 61.190491, -149.868937, content)
+        new mapPin("Charlies Bakery", 61.196148, -149.885577),
+        new mapPin("Moose's Tooth", 61.190491, -149.868937)
     ])
 };
 
 // Initiates the viewModel bindings.
 
-var content = document.createElement('div')
-content.id = "content"
-
-var apiData = function() {
-
-    var wikipediaURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + name + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function () {
-        $content.text("Failed to get Wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-
-        url: wikipediaURL,
-        dataType: "jsonp",
-
-        success: function (response) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $content.append('<li><a href ="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
-
-}
-
 ko.applyBindings(viewModel);
-apiData();
