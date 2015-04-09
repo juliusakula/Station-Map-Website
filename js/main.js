ea8b5d3eceb27c533b3dce5d18746bc80dda3a90
James Ives <james@jimo.co.uk>
2015-04-09 12:57:42 -0800

    var update = 7;
    More updates.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // The mapPin constructor class takes in logic from the View Model to display the pins you see on the map.
 
-function mapPin(name, lat, long) {
+function mapPin(name, lat, long, text) {
 
     this.name = ko.observable(name);
     this.lat = ko.observable(lat);
     this.long = ko.observable(long);
+    this.text = ko.observable(text);
 
     // This initializes the markers on the map. It also adds an annimation too.
 
@@ -16,12 +17,6 @@ function mapPin(name, lat, long) {
 
     // This initalizes the info window when the markers are clicked. It also creates a div for the API data.
 
-    var contentString = '<div data-bind="" id="wikiData"></div>';
-
-    var infoWindow = new google.maps.InfoWindow({
-        content: contentString
-
-    });
 
     // This function causes the markers to bounce when they are clicked. 
 
@@ -34,17 +29,25 @@ function mapPin(name, lat, long) {
         }
     }
 
+    function article(content, url) {
+
+        var self = this;
+        self.content = content;
+        self.url = url;
+
+    }
+
     // This function pulls in data from the WikiPedia API.
 
-    apiData = function() {
+    function apiData() {
+        
+        //TODO : Fix fail request.
 
-        var $wikiData = $("#wikiData");
         var wikipediaURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + name + '&format=json&callback=wikiCallback';
+        var wikiFailText = 'Failed to get Wikipedia resources';
         var wikiRequestTimeout = setTimeout(function () {
-            $wikiData.text("Failed to get Wikipedia resources");
-        }, 500);
-
-        $wikiData.text("");
+            viewModel.articleList.push(new article(wikiFailText, wikiFailText));
+        }, 5);
 
         $.ajax({
 
@@ -52,12 +55,15 @@ function mapPin(name, lat, long) {
             dataType: "jsonp",
 
             success: function (response) {
+
+                viewModel.articleList.removeAll();
+
                 var articleList = response[1];
 
                 for (var i = 0; i < articleList.length; i++) {
                     articleStr = articleList[i];
                     var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                    $wikiData.append('<li><a href ="' + url + '">' + articleStr + '</a></li>');
+                    viewModel.articleList.push(new article(articleStr, url));
                 }
 
                 clearTimeout(wikiRequestTimeout);
@@ -66,12 +72,21 @@ function mapPin(name, lat, long) {
 
     }
 
-    // Calls the animations and info window.
+    var contentString = '<li data-bind="foreach: articleList"><a data-bind="attr: {href: url}, text: content"></a></li>';
+    var infowindow = new google.maps.InfoWindow({});
     
-    google.maps.event.addListener(marker, 'click', toggleBounce);
+    google.maps.event.addListener(mapView, 'click', function () {
+        infowindow.close();
+    });
+
     google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.close();
-        infoWindow.open(mapView, marker);
+        infowindow.close();
+        toggleBounce();
+        infowindow = new google.maps.InfoWindow({
+            content: text + contentString
+        });
+
+        infowindow.open(mapView, marker);
         apiData();
     });
 
@@ -88,11 +103,29 @@ var mapView = new google.maps.Map(document.getElementById('map-canvas'), {
 
 var viewModel = {
 
+    articleList: ko.observableArray([]),
+
     pins: ko.observableArray([
-        new mapPin("Charlies Bakery", 61.196148, -149.885577),
-        new mapPin("Moose's Tooth", 61.190491, -149.868937)
+        new mapPin("Alaska Communications", 61.196148, -149.885577, "test11"),
+        new mapPin("Anchorage Alaska", 61.190491, -149.868937, "test2")
     ]),
+
+    // TODO
+
+    query: ko.observable(''),
+    
+
+    search: function (value) {
+        viewModel.pins[0].removeAll();
+
+        for (var i in pins) {
+            if (pins[i].name.toLowerCase().indexOf(valkue.toLowerCase()) >= 0) {
+                this.pins.push(pins[i])
+            }
+        }
+    }
 };
+
 
 // Initiates the viewModel bindings.
 
