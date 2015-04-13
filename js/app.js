83c5973ef3f2c2c93a3b4f342ad292f41bd2bc8a
James Ives <James@Jimo.co.uk>
2015-04-12 18:42:09 -0800

    var update = 17;
    - Adding self.pan, currently broken.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ function Model() {
 			var url = articles[i].url;
 				
 			if (articles[i].error) {
-				Info_Content += '<h4 style="color: red">' + content + '</h4>';
+				Info_Content += '<h5 style="color: red">' + content + '</h5>';
 			}
 				
 			else {
@@ -82,7 +82,7 @@ var viewModel = function () {
 
         // These are the functions that are called when the map markers are clicked.
 
-        this.click = google.maps.event.addListener(marker, 'click', function () {
+        google.maps.event.addListener(marker, 'click', function () {
             // Send AJAX request first
             self.apiData(name);
 
@@ -100,8 +100,19 @@ var viewModel = function () {
       new self.mapPin("King's Cross Station", 51.530984, -0.122583, "King's Cross railway station is a major London railway terminus, opened in 1852. It is on the northern edge of central London, at the junction of Euston Road and York Way, in the London Borough of Camden on the boundary with the London Borough of Islington. It is one of 19 stations managed by Network Rail."),
       new self.mapPin("Putney Bridge", 51.468050, -0.209081, "Putney Bridge is a London Underground station on the Wimbledon branch of the District line. It is between Parsons Green and East Putney stations and is in Zone 2. The station is located in the south of Fulham, adjacent to Fulham High Street and New Kings Road (A308) and is a short distance from the north end of Putney Bridge from which it takes its name."),
       new self.mapPin("Piccadilly Circus", 51.510271, -0.134211, "Piccadilly Circus is a London Underground station located directly beneath Piccadilly Circus itself, with entrances at every corner. Located in Travelcard Zone 1, the station is on the Piccadilly line between Green Park and Leicester Square and on the Bakerloo line between Charing Cross and Oxford Circus."),
-      new self.mapPin("Embankment Station", 51.507225, -0.122215, "Embankment is a London Underground station in the City of Westminster, known by various names during its history. It is served by the Circle, District, Northern and Bakerloo lines. On the Northern and Bakerloo lines, the station is between Waterloo and Charing Cross stations; on the Circle and District lines, it is between Westminster and Temple and is in Travelcard Zone 1. The station has two entrances, one on Victoria Embankment and the other on Villiers Street. The station is adjacent to Victoria Embankment Gardens and is close to Charing Cross station, Embankment Pier, Hungerford Bridge, Cleopatra's Needle, the Royal Air Force Memorial, the Savoy Chapel and Savoy Hotel and the Playhouse and New Players Theatres.")
+      new self.mapPin("Embankment Tube Station", 51.507225, -0.122215, "Embankment is a London Underground station in the City of Westminster, known by various names during its history. It is served by the Circle, District, Northern and Bakerloo lines. On the Northern and Bakerloo lines, the station is between Waterloo and Charing Cross stations; on the Circle and District lines, it is between Westminster and Temple and is in Travelcard Zone 1. The station has two entrances, one on Victoria Embankment and the other on Villiers Street. The station is adjacent to Victoria Embankment Gardens and is close to Charing Cross station, Embankment Pier, Hungerford Bridge, Cleopatra's Needle, the Royal Air Force Memorial, the Savoy Chapel and Savoy Hotel and the Playhouse and New Players Theatres.")
     ]);
+	
+	// self.pan is currently broken.
+	
+	self.pan = function () {
+		
+		for (var i = 0; i < self.pins.length; i++) {
+			console.log("im being clicked");
+			map.panTo(marker.position);
+			break;
+		}
+	};
 
     self.query = ko.observable('');
 
@@ -116,6 +127,7 @@ var viewModel = function () {
             return doesMatch;
         });
     });
+	
 
     self.apiData = function (name) {
 
@@ -137,7 +149,6 @@ var viewModel = function () {
             },
             error: function () {
 				self.articleList.removeAll();
-				console.log("Removed all and triggered error true.")
 				var errorResponse = "Unable to contact the Wikipedia database. Select another map marker to try again."
                 self.articleList.push(new self.article(errorResponse, null, true));
             }
@@ -150,5 +161,10 @@ var viewModel = function () {
 
 // Initiates the viewModel bindings.
 $(document).ready(function () {
+	
+	if (!mapLoaded) alert("Google Maps has failed to load. Please refresh the page to try again.");
+	
     ko.applyBindings(new viewModel());
+	
 });
+
